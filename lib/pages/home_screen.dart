2657a995c162eb94658348f25b3ad6cbd8834937
Gabriel <gbielacerda.m@gmail.com>
2025-02-26 06:51:46 -0300

    install flutter_svg

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:focus_fields/pages/challenge_tab.dart';
+import 'package:focus_fields/pages/chart.dart';
+import 'package:focus_fields/pages/farm.dart';
+import 'package:focus_fields/pages/profile.dart';
+import 'package:focus_fields/pages/tools.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = const [
+    ChallengeTab(),
+    Chart(),
+    Farm(),
+    Profile(),
+    Tools(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Focus Fields'),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset(
+              'assets/icons/coins.svg',
+              height: 24,
+              width: 24,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: SvgPicture.asset(
+              'assets/icons/clover.svg',
+              height: 24,
+              width: 24,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/list-todo.svg'),
+            label: 'Challenge',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/chart-spline.svg'),
+            label: 'Chart',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/farm.svg'),
+            label: 'Farm',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/user.svg'),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/hammer.svg'),
+            label: 'Tools',
+          ),
+        ],
+      ),
+    );
+  }
+}
